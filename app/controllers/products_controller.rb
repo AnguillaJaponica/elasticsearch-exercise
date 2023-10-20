95f7f4ae3de21acb786c5552203ddbf4852de80d
Daiki Matoba <telnetstat@gmail.com>
2023-10-20 11:27:44 +0900

    Implement basic search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,22 @@
 class ProductsController < ApplicationController
   def index
+    @query = params[:query]
     @products = Product.all
+
+    return if @query.blank?
+
+    client = OpenSearch::Client.new(host: 'localhost:9200')
+    @response = client.search(
+      index: Product::INDEX_NAME,
+      body: {
+        query: {
+          match: {
+            name: @query
+          }
+        }
+      }
+    )
+    ids = @response['hits']['hits'].pluck('_id')
+    @products = @products.where(id: ids)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,23 @@
 class Product < ApplicationRecord
+  INDEX_NAME = 'products'
+
+  def self.import
+    client = OpenSearch::Client.new(host: 'localhost:9200')
+
+    client.indices.delete(index: INDEX_NAME) rescue nil
+    client.indices.create(index: INDEX_NAME)
+
+    self.find_each do |product|
+      client.index(
+        index: INDEX_NAME,
+        id: product.id,
+        body: {
+          name: product.name,
+          category: product.category,
+          average_rating: product.average_rating,
+          price: product.price,
+        },
+      )
+    end
+  end
 end
