551ded67c29e73f979da538c1719a63a9597d3da
Daiki Matoba <telnetstat@gmail.com>
2023-10-28 23:19:24 +0900

    Create index by bulk-operations

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,17 +7,22 @@ class Product < ApplicationRecord
     client.indices.delete(index: INDEX_NAME) rescue nil
     client.indices.create(index: INDEX_NAME)
 
-    self.find_each do |product|
-      client.index(
-        index: INDEX_NAME,
-        id: product.id,
-        body: {
-          name: product.name,
-          category: product.category,
-          average_rating: product.average_rating,
-          price: product.price,
-        },
-      )
+    self.find_in_batches do |products|
+      actions = products.map do |product|
+        {
+          index: {
+            _index: INDEX_NAME,
+            _id: product.id,
+            data: {
+              name: product.name,
+              category: product.category,
+              average_rating: product.average_rating,
+              price: product.price,
+            }
+          }
+        }
+      end
+      client.bulk(body: actions)
     end
   end
 end
